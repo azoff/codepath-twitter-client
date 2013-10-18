b17d38fece5ff540947df39fc31e7261e52565c7
Jonathan Azoff <jon@azoffdesign.com>
2013-10-18 00:48:52 -0700

    Corrected libs path, added login activity

---
+++ TwitterClient/src/main/java/com/example/twitterclient/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.twitterclient.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+import com.example.twitterclient.R;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+}
