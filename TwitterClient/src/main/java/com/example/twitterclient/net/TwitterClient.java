a5b0fc307922dd0a527b430bb67990582f44d6ba
Jonathan Azoff <jon@azoffdesign.com>
2013-10-20 12:24:10 -0700

    Added getHomeTimeline method

--- TwitterClient/src/main/java/com/example/twitterclient/net/TwitterClient.java
+++ TwitterClient/src/main/java/com/example/twitterclient/net/TwitterClient.java
@@ -2,6 +2,7 @@ package com.example.twitterclient.net;
 
 import android.content.Context;
 import com.codepath.oauth.OAuthBaseClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import org.scribe.builder.api.Api;
 import org.scribe.builder.api.TwitterApi;
 
@@ -18,6 +19,11 @@ public class TwitterClient extends OAuthBaseClient {
 
 	public TwitterClient(Context context) {
 		super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
+	}
+
+	public void getHomeTimeline(AsyncHttpResponseHandler handler) {
+		String url = getApiUrl("statuses/home_timeline.json");
+		client.get(url, handler);
 	}
 
     /* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
